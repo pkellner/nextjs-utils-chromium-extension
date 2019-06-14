23e1750ddf16896f50abf7111171a0cbc4ad2281
pkellner <peter@peterkellner.net>
2019-06-14 06:40:41 -0700

    wip

--- nextjs1.js
+++ nextjs1.js
@@ -5,12 +5,12 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   const versionScript = document.createElement("script");
   console.log(`message:${JSON.stringify(message)}`);
   if (message.action === "updateBadgeText") {
-    versionScript.src = chrome.runtime.getURL("inject-script.js");
+    versionScript.src = chrome.runtime.getURL("inject-script-badgetext.js");
     versionScript.onload = function autoUnload() {
       this.remove;
     };
   } else if (message.action === "showNextJsData") {
-    versionScript.src = chrome.runtime.getURL("inject-script.js");
+    versionScript.src = chrome.runtime.getURL("inject-script-showdata.js");
     versionScript.onload = function autoUnload() {
       this.remove;
     };
@@ -25,9 +25,24 @@ window.addEventListener(
     // We only accept messages from ourselves
     if (event.source != window) return;
 
-    if (event.data.type && event.data.type == "FROM_PAGE") {
+    if (event.data.type && event.data.type == "BADGE_TEXT") {
       chrome.runtime.sendMessage(
         {
+          messageType: event.data.type,
+          nextJsData: event.data.nextJsData,
+          nextJsDataLength: event.data.nextJsData.length
+        },
+        function(response) {
+          // for now, nothing to do here.  just sending message and return will
+          //   come frm a sendMessage in inject-script.js
+        }
+      );
+    }
+
+    if (event.data.type && event.data.type == "SHOW_DATA") {
+      chrome.runtime.sendMessage(
+        {
+          messageType: event.data.type,
           nextJsData: event.data.nextJsData,
           nextJsDataLength: event.data.nextJsData.length
         },
