4c311f8a061de4e0a4cd0fba09bbb9df3c53d46d
pkellner <peter@peterkellner.net>
2019-06-14 12:32:27 -0700

    working

--- viewNextData.js
+++ viewNextData.js
@@ -1,5 +1,12 @@
-$( document ).ready(function() {
-  debugger;
-  const nextData = window.localStorage.getItem('nextdata');
-  $("#nextdataid").html(nextData);
+$(document).ready(function() {
+  const nextData = window.localStorage.getItem("nextdata");
+  const obj = JSON.parse(nextData);
+  document.body.innerHTML = "";
+  document.body.appendChild(
+    document.createTextNode(JSON.stringify(obj, null, 4))
+  );
+  // $("#nextdataid").html(nextData);
 });
+
+// document.body.innerHTML = "";
+// document.body.appendChild(document.createTextNode(JSON.stringify(obj, null, 4)));
